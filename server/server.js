9c4e52b1824bb2ad960f6935142f95aa34c350d6
Abhisekh Kayastha <abhisekhkayasth@gmail.com>
2024-05-16 09:15:23 +0545

    Connected Mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
 const app = require('./app');
+const mongoose = require('./mongoose')
 const PORT = process.env.PORT || 8080;
+require('dotenv').config();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
 });
